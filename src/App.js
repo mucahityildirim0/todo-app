edf4997f60e9e4e050670891cc56cc20caebdbc9
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-06-25 16:49:46 +0300

    I used usestate and map function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState([
+    "take dogs for a walk",
+    "take the rubbish out",
+    "livestream today",
+  ]);
+
+  return (
+    <div className="App">
+      <h1>Hello Programmers!!</h1>
+      <input type="text" />
+      <button>Add todo</button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
